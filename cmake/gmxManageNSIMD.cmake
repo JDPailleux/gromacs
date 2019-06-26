da6562c6ec38a5e04a668c754f51c186d3dfd860
jdpailleux <jean-di.pailleux@outlook.com>
2019-06-26 16:31:20 +0200

    Update of Cmake

--- cmake/gmxManageNSIMD.cmake
+++ cmake/gmxManageNSIMD.cmake
@@ -3,7 +3,8 @@
 
 # Set variables path 
 set(GMX_NSIMD_ROOT "/home/jpailleux/Bureau/nsdev/nsimd")
-set(GMX_NSIMD_INCLUDE_PATH "${GMX_NSIMD_ROOT}/include")
+set(GMX_NSIMD_INCLUDE_PATH1 "${GMX_NSIMD_ROOT}/include")
+set(GMX_NSIMD_INCLUDE_PATH2 "${GMX_NSIMD_ROOT}/nsimd/include")
 set(GMX_NSIMD_LIBRARY_PATH "${GMX_NSIMD_ROOT}/build")
 
 #set(GMX_NSIMD_FLAGS "-I$(GMX_NSIMD_INC) -L$(GMX_NSIMD_LIB) -lnsimd")
@@ -13,8 +14,10 @@ find_library(NSIMD_LIBRARY "libnsimd_x86_64")
 
 set(NSIMD_COMPILE_FLAGS "-DAVX -mavx2")
 set(NSIMD_LINKER_FLAGS "-L${GMX_NSIMD_ROOT}/build -lnsimd_x86_64")
-include_directories(SYSTEM ${GMX_NSIMD_INCLUDE_PATH})
+#-I${GMX_NSIMD_INCLUDE_PATH1} -I${GMX_NSIMD_INCLUDE_PATH2}
+include_directories(SYSTEM ${GMX_NSIMD_INCLUDE_PATH1} ${GMX_NSIMD_INCLUDE_PATH2})
 if (NSIMD_LIBRARY)
   list(APPEND GMX_EXTRA_LIBRARIES ${NSIMD_LIBRARY})
+  set(GMX_SIMD_NSIMD 1)
 endif()
-
+# Il faut defined la macro de NSIMD !!!!!!
